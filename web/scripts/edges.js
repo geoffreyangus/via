a78b703ff3f57b9137ce12b8d7cc299a13584166
elzwang <ewang3@stanford.edu>
2019-11-19 02:28:53 -0800

    small bug in node.highlighted style, simplified edges opacity

--- web/scripts/edges.js
+++ web/scripts/edges.js
@@ -2,10 +2,7 @@ function styleEdges() {
     cy.style().selector('edge').style({
         'width': 'data(width)',
         'line-color': 'data(color)',
-        'opacity': function(ele) {
-            return Math.min(0.3,ele.data('weight'));
-        },
-        // 'opacity': 'mapData(weight, 0, 100, 0, 0.3)',
+        'opacity': 'data(weight)',
         'target-arrow-color': 'data(color)',
         'target-arrow-shape': 'triangle',
         'curve-style': 'bezier'
